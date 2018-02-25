9d0b7cbc2ff630bcda26e8cf5777b3f31bcb8783
zhengyuxin <zhengyuxin0507@163.com>
2018-02-25 22:45:47 +0800

    all

---
+++ build/CMakeFiles/flag-recognize.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flag-recognize.dir/flag-recognize.o"
+  "flag-recognize.pdb"
+  "flag-recognize"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flag-recognize.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
